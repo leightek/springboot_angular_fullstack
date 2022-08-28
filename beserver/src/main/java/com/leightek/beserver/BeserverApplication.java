7a830267c5c42a7adbb1c3a8959097f3d44fe10f
leightek <leighsuconsulting@gmail.com>
2022-08-28 00:30:11 -0700

    [SpringBootAngular] Created backend server app

---
+++ beserver/src/main/java/com/leightek/beserver/BeserverApplication.java
@@ -0,0 +1,13 @@
+package com.leightek.beserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeserverApplication.class, args);
+	}
+
+}
---
+++ beserver/src/test/java/com/leightek/beserver/BeserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leightek.beserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
