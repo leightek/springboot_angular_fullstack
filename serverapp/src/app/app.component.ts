1ebf736ca9460500d7fb195b24d9cd27b86a0074
leightek <leighsuconsulting@gmail.com>
2022-09-27 22:34:17 -0700

    [SpringBootAngular] Updated app component page with conditional container display

--- serverapp/src/app/app.component.ts
+++ serverapp/src/app/app.component.ts
@@ -13,6 +13,8 @@ import { ServerService } from './service/server.service';
 })
 export class AppComponent implements OnInit {
   appState$: Observable<AppState<CustomResponse>>;
+  readonly DataState = DataState;
+
   constructor(private serverService: ServerService) {}
 
   ngOnInit(): void {
